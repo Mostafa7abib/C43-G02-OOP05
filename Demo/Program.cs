a630f12679d3ba335237f6ae22033499f10085a7
Mostafa Habib <momosasahabib9@gmail.com>
2025-01-05 14:07:57 +0200

    Video 1 - Relational Operators Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            #region Operators Overloading _ Relational
+            Complex C1 = new Complex(Real = 1, Imag = 2);
+            Complex C2 = new Complex(Real = 2, Imag = 4);
+            if (C1 > C2)
+                Console.WriteLine("C1 Is Greater");
+            else 
+                Console.WriteLine("C2 Is Greater");
+
+            if (C1 == C2)  // == Is compare ref to ref (not objrct state to object state)
+                Console.WriteLine("C1 == C2");
+            else 
+                Console.WriteLine("C1 != C2");
+            #endregion
+
         }
     }
 }
