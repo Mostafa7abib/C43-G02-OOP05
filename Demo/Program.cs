228e6d8c1c8e91f41ba0fe46aae77dfd3dd06ee1
Mostafa Habib <momosasahabib9@gmail.com>
2025-01-05 14:28:34 +0200

    Video 2 - Casting Operators Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,19 +5,29 @@
         static void Main()
         {
             #region Operators Overloading _ Relational
-            Complex C1 = new Complex(Real = 1, Imag = 2);
-            Complex C2 = new Complex(Real = 2, Imag = 4);
-            if (C1 > C2)
-                Console.WriteLine("C1 Is Greater");
-            else 
-                Console.WriteLine("C2 Is Greater");
+            //Complex C1 = new Complex(Real = 1, Imag = 2);
+            //Complex C2 = new Complex(Real = 2, Imag = 4);
+            //if (C1 > C2)
+            //    Console.WriteLine("C1 Is Greater");
+            //else 
+            //    Console.WriteLine("C2 Is Greater");
 
-            if (C1 == C2)  // == Is compare ref to ref (not objrct state to object state)
-                Console.WriteLine("C1 == C2");
-            else 
-                Console.WriteLine("C1 != C2");
+            //if (C1 == C2)  // == Is compare ref to ref (not objrct state to object state)
+            //    Console.WriteLine("C1 == C2");
+            //else 
+            //    Console.WriteLine("C1 != C2");
             #endregion
 
+            #region Casting Operators Overloading 
+            object obj = 4;
+            int y = (int)obj; // Explicit Casting
+            Complex complex = new Complex() { real = 10, imag = 20 };
+            int X = (int)complex;
+            Console.WriteLine(X);
+
+            string s = complex;
+            Console.WriteLine(s);
+            #endregion
         }
     }
 }
